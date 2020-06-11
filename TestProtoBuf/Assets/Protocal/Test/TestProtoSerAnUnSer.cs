df032df63d4b705a99a54582e97a9ca397f41ccb
gucongyi <gucongyii@163.com>
2020-06-11 14:56:05 +0800

    测试序列化，反序列化

---
+++ TestProtoBuf/Assets/Protocal/Test/TestProtoSerAnUnSer.cs
@@ -0,0 +1,32 @@
+﻿using Protobuf;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProtoSerAnUnSer : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1.序列化
+        StoreRequest storeRequest = new StoreRequest();
+        storeRequest.Name = "goods";
+        storeRequest.Num = 50;
+        storeRequest.Result = 1;
+        for (int i = 0; i < 50; i++)
+        {
+            string str = $"str{i + 1}";
+            storeRequest.MyList.Add(str);
+        }
+        byte[] bytes=ProtobufTool.Serialize(storeRequest);
+        //2.反序列化
+        StoreRequest storeRequestUnSer = ProtobufTool.Deserialize<StoreRequest>(bytes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
